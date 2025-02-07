032e2f07e2e094b8254a5abfe2136fc93dfbb263
Swapvp <swapneshpawar1994@gmail.com>
2025-02-07 18:12:27 +0530

    start-it-hero

--- start-it-up/app/(root)/page.tsx
+++ start-it-up/app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
 
-export default function Home() {
+import SearchForm from "../../components/SearchForm"
+
+export default async function Home({searchParams}:{searchParams: Promise<{ query?: string}>}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-center text-lg font-semibold text-lg"> HOME</h1>
+    <section className="pink_container">
+
+      <h1 className="heading"> Submit Your Start up <br /> Connect With Entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Start ups, and Get Noticed</p>
+
+    <SearchForm/>
+    </section>
     </>
   );
 }
